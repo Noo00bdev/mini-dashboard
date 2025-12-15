e28d12dc6d700bd9ddc29babdaf65919fd4deaa9
Noo00bdev <koffiyvann.200@gmail.com>
2025-12-15 19:44:33 +0000

    Automate commit

--- scripts.js
+++ scripts.js
@@ -388,7 +388,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const recTasks = document.querySelector('#recTask');
     if (recTasks) {
         let recTask = localStorage.getItem('tasks');
-        if (!(!recTask || recTask === '[]')) {
+        if (!recTask || recTask === '[]') {
+            taskContainer.innerHTML = '<p class="text-center text-gray-500 p-4">Aucune ancienne tâche.';
+        }else{
             recTask = JSON.parse(recTask);
             recTasks.innerHTML = '';
             const newTasks = recTask.slice(0,2)
@@ -397,21 +399,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 div.className = 'bg-white p-4 rounded-xl mb-3 shadow-sm border border-gray-200';
 
                 div.innerHTML = `
-                    <div class="flex justify-between items-start gap-4">
-                        <div class="flex-1 min-w-0">
-                            <h3 class="font-bold text-lg mb-2 text-text-primary">${task.title}</h3>
-                            <p class="text-gray-600">${task.description}</p>
-                        </div>
-                        <button class="delete-task shrink-0 text-red-500 hover:text-red-700 hover:bg-red-50 p-3 rounded-lg transition-colors" data-id="${task.id}">
-                            <i class="fa-solid fa-trash text-lg pointer-events-none"></i>
-                        </button>
-                    </div>`;
+                <div class="flex justify-between items-start gap-4">
+                    <div class="flex-1 min-w-0">
+                        <h3 class="font-bold text-lg mb-2 text-text-primary">${task.title}</h3>
+                        <p class="text-gray-600">${task.description}</p>
+                    </div>
+                    <button class="delete-task shrink-0 text-red-500 hover:text-red-700 hover:bg-red-50 p-3 rounded-lg transition-colors" data-id="${task.id}">
+                        <i class="fa-solid fa-trash text-lg pointer-events-none"></i>
+                    </button>
+                </div>`;
                 recTasks.appendChild(div);
-
             })
-
-
         }
+
+
     }
 
 });
